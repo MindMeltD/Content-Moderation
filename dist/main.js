b028c82b0bde35f32bd69fbdb1f80481aea24e88
Ian G <iangordon3@hotmail.com>
2025-05-26 16:24:00 -0400

    Testing fix to persistent build issues

--- dist/main.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const App = () => React.createElement("h1", null, "Hello from React!");
-ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(App, null));
