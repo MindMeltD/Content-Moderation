14fc7f0f50669db00a15d911f159e2fb1411ecd2
Ian G <iangordon3@hotmail.com>
2025-05-26 21:09:27 -0400

    Testing changes to build parameters

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-    // base: './',
+    base: './',
+    plugins: [react()],
     build: {
         outDir: 'dist',
         emptyOutDir: false,
